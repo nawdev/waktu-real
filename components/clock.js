59e940e570ab4751344aea38b82dcc604e260814
nawdev <154863645+nawdev@users.noreply.github.com>
2024-01-19 06:16:14 +0700

    Update clock.js

--- components/clock.js
+++ components/clock.js
@@ -1,1 +1,25 @@
+// components/Clock.js
+import { useEffect, useState } from 'react';
+import moment from 'moment';
+
+const Clock = () => {
+  const [currentTime, setCurrentTime] = useState(moment().format('HH:mm:ss'));
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentTime(moment().format('HH:mm:ss'));
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div>
+      <h1>Real-time Clock</h1>
+      <p>{currentTime}</p>
+    </div>
+  );
+};
+
+export default Clock;
 
